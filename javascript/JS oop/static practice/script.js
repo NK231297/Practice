f36d7ab3bb109ea78617d289b84a5611af32aaf3
Nikhil Kumar <nk231297@gmail.com>
2025-06-24 09:48:00 +0530

    Auto backup on 2025-06-24 09:48:00

--- javascript/JS oop/static practice/script.js
+++ javascript/JS oop/static practice/script.js
@@ -24,6 +24,15 @@ class BankAccount {
 
 let accounts = [];
 
-accounts.push(BankAccount.account(`Mahesh Dalle`, 2000))
+accounts.push(BankAccount.account(`Vibhuti`, 1500))
+accounts.push(BankAccount.account(`Manmohan`, 2000))
+accounts.push(BankAccount.account(`Happu`, 2000))
+accounts.push(BankAccount.account(`Prem`, 2000))
+accounts.push(BankAccount.account(`Tika`, 100))
+accounts.push(BankAccount.account(`Malkhan`, 150))
+accounts.push(BankAccount.account(`Tillu`, 250))
+accounts.push(BankAccount.account(`Master`, 650))
+accounts.push(BankAccount.account(`Doctor`, 250))
+accounts.push(BankAccount.account(`Chacha`, 2500))
 
 console.log(accounts)
