fcba07db8733352b093eb6a89ad9ba57f6630bb1
Nikhil Kumar <nk231297@gmail.com>
2025-06-24 09:26:00 +0530

    Auto backup on 2025-06-24 09:26:00

---
+++ javascript/JS oop/static practice/script.js
@@ -0,0 +1,19 @@
+class BankAccount {
+    customerName;
+    balance = 0;
+    accountNumber;
+
+    constructor(customerName, balance = 0){
+        this.customerName = customerName;
+
+        this.balance = balance;
+
+        this.accountNumber = Date.now();
+    };
+
+
+};
+
+let acc1 = new BankAccount(`Nikhil K`, 1000);
+
+console.log(acc1);
