cd01dab106acf3b8de432731eca0d13ec2184528
Nikhil Kumar <nk231297@gmail.com>
2025-06-26 07:52:00 +0530

    Auto backup on 2025-06-26 07:52:00

--- javascript/JS oop/static practice/script.js
+++ javascript/JS oop/static practice/script.js
@@ -12,8 +12,29 @@ class bankAccount{
 
         this.accountNumber = Date.now();
     };
+
+    deposit(amount){
+        this.balance += amount;
+    };
+
+    withdraw(amount){
+        this.balance -= amount;
+    };
 };
 
-let acc1 = new bankAccount(`Nikhil K`, 1000);
+let customerName = document.getElementById("name");
+let balance = document.getElementById("balance");
+let savings = document.getElementById("savings");
+let current = document.getElementById("current");
 
-console.log(acc1)
+let accounts = [];
+
+savings.addEventListener(`click`, function(e){
+    e.preventDefault();
+
+    let acc = new bankAccount(customerName.value, +balance.value);
+
+    accounts.push(acc);
+
+    console.log(accounts);
+})
