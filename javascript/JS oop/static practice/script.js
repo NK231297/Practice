a5fca347a4f9d2408555daa19915ae7c7b6742e8
Nikhil Kumar <nk231297@gmail.com>
2025-06-26 20:40:00 +0530

    Auto backup on 2025-06-26 20:40:00

--- javascript/JS oop/static practice/script.js
+++ javascript/JS oop/static practice/script.js
@@ -12,35 +12,54 @@ class bankAccount{
 
         this.accountNumebr = Date.now()
     };
+
+    deposit(amount){
+        this.balance += amount;
+    };
+
+    withdraw(amount){
+        this.balance -= amount;
+    };
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 let customerName = document.getElementById("name");
 let balance = document.getElementById("balance");
-let savings = document.getElementById("savings");
-let current = document.getElementById("current");
-
+let savingsBT = document.getElementById("savings");
+let currentBT = document.getElementById("current");
 
 let accounts = [];
 
+savingsBT.addEventListener( `click`, function(e){
+    e.preventDefault()
 
+    let account = new bankAccount(customerName.value, +balance.value);
+
+    accounts.push(account);
+
+    console.log(accounts);
+} )
+
+currentBT.addEventListener( `click`, function(e){
+    e.preventDefault()
+
+    let account = new bankAccount(customerName.value, +balance.value);
+
+    accounts.push(account);
+
+    console.log(accounts);
+} )
 
 let accNo = document.getElementById(`account`);
 let amount = document.getElementById(`amount`);
 let depositBT = document.getElementById(`deposit`);
 let withdrawBT = document.getElementById(`withdraw`);
+
+depositBT.addEventListener( `click`, function(e){
+    e.preventDefault()
+
+    let account = accounts.find( (account)=>account.accountNumebr === +accNo.value )
+
+    account.deposit(+amount.value);
+
+    console.log(accounts);
+} )
