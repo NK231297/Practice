d73db305588e6edaec82e921702f9787fe1d0984
Nikhil Kumar <nk231297@gmail.com>
2025-06-30 08:12:00 +0530

    Auto backup on 2025-06-30 08:12:00

--- javascript/Chapter 11 practice set/script.js
+++ javascript/Chapter 11 practice set/script.js
@@ -5,9 +5,15 @@ class complexN{
     constructor(realN, imagenaryN){
         this.realN = realN;
         this.imagenaryN = imagenaryN;
+        return new complexN(realN, imagenaryN)
+    };
+
+    add(num){
+        this.realN += num;
+        this.ImagenaryN += num;
     };
 };
 
 let num1 = new complexN(2, 4);
 
-console.log(`${num1.realN}, ${num1.imagenaryN}`);
+console.log(num1);
