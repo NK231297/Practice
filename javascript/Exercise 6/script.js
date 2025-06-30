807b13f729649b798d6a33c4a275f5add6250487
Nikhil Kumar <nk231297@gmail.com>
2025-06-30 20:48:00 +0530

    Auto backup on 2025-06-30 20:48:00

--- javascript/Exercise 6/script.js
+++ javascript/Exercise 6/script.js
@@ -1,4 +1,19 @@
 class todo{
+    static id = 1;
     discription;
-    
-}
+
+    constructor(discription){
+        this.id = todo.id++;
+        this.discription = discription;
+    }
+};
+
+let discInput = document.getElementById(`disc`);
+let saveBT = document.getElementById(`save`);
+
+
+saveBT.addEventListener(`click`, function(e){
+    e.preventDefault();
+
+    localStorage.setItem(id.value, discInput.value)
+});
