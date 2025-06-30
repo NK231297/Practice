d9ededf12a62cd06f62959fe264fdd68052af6f0
Nikhil Kumar <nk231297@gmail.com>
2025-06-30 07:58:00 +0530

    Auto backup on 2025-06-30 07:58:00

---
+++ javascript/Chapter 11 practice set/script.js
@@ -0,0 +1,13 @@
+class complex{
+    realN;
+    compN;
+
+    constructor(realN, compN){
+        this.realN = realN;
+        this.compN = compN;
+    };
+};
+
+let num1 = new complex(2, 4);
+
+console.log(num1);
