5bab0d12e9b0c4cc1c3d6bf5fd997878defac450
NK231297 <nk231297@gmail.com>
2025-07-09 10:37:26 +0530

    Auto backup at 09-07-2025 10:37:26.46

--- super_simple_dev/05-Variables/07-Functions/script.js
+++ super_simple_dev/05-Variables/07-Functions/script.js
@@ -1,3 +1,4 @@
+
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissor = document.getElementById('scissor');
@@ -6,26 +7,72 @@ let yourScore = 0;
 let computerScore = 0;
 let Draw = 0;
 
-let choice = ["rock", "paper", "Scissor"];
+function abc(){
+    computerChoice = Math.floor(Math.random() * 3) + 1
+    
+    computerChoice === 1 ? console.log(`Comp. Move: Rock`) : 
+    computerChoice === 2 ? console.log(`Comp. Move: Paper`) : console.log(`Comp. Move: Scissor`);
+
+    let userChoice = choiceNumber;
+
+    let computerMove = computerChoice;
+
+    let decision =  userChoice === 1 && computerMove === 2 ? "Computer Win" :
+                    userChoice === 1 && computerMove === 3 ? "You Win" :
+                    userChoice === 2 && computerMove === 1 ? "You Win" :
+                    userChoice === 2 && computerMove === 3 ? "Computer Win" :
+                    userChoice === 3 && computerMove === 2 ? "You Win" :
+                    userChoice === 3 && computerMove === 1 ? "Computer Win" : "Draw";
+
+    console.log(decision);
+
+    return 5;
+};
+
 
 let choiceNumber = 0;
+
+let computerChoice = "";
 
 rock.addEventListener(`click`, function(e){
     e.preventDefault();
 
-    choiceNumber++;
+    choiceNumber === 0 ? choiceNumber++ : console.log("");
+
+    console.log("User Move: Rock");
+
+    console.log(abc());
 })
 
 paper.addEventListener(`click`, function(e){
     e.preventDefault();
+    choiceNumber === 0 ? choiceNumber += 2 : console.log("");
+    
+    console.log("User Move: Paper");
 
-    choiceNumber += 2;
+    console.log(abc());
 })
 
 scissor.addEventListener(`click`, function(e){
     e.preventDefault();
+    choiceNumber === 0 ? choiceNumber += 3 : console.log("");
 
-    choiceNumber += 3;
+    console.log("User Move: Scissor");
+
+    console.log(abc());
 })
 
-let userChoice = 
+
+/*
+function calculateTax (cost, tax){
+    console.log(cost * tax);
+};
+
+calculateTax(1000, 0.18);
+
+function discPercent(cost, nettRate){
+    return ((cost - nettRate) / cost) * 100 + "%";
+};
+
+discPercent(1000, 850);
+*/
