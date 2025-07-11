614421bc5b6f0a108cde6b1e0c7936a35f509c12
NK231297 <nk231297@gmail.com>
2025-07-11 22:08:16 +0530

    Auto backup at 11-07-2025 22:08:16.17

--- super_simple_dev/07-Functions/Chalange Exercise/script.js
+++ super_simple_dev/07-Functions/Chalange Exercise/script.js
@@ -1,133 +1,108 @@
-const num1 = document.getElementById(`num1`);
-const num2 = document.getElementById(`num2`);
-const num3 = document.getElementById(`num3`);
-const num4 = document.getElementById(`num4`);
-const num5 = document.getElementById(`num5`);
-const num6 = document.getElementById(`num6`);
-const num7 = document.getElementById(`num7`);
-const num8 = document.getElementById(`num8`);
-const num9 = document.getElementById(`num9`);
-const num0 = document.getElementById(`num0`);
-const plus = document.getElementById(`plus`);
-const minus = document.getElementById(`minus`);
-const multiply = document.getElementById(`multiply`);
-const divide = document.getElementById(`divide`);
-const equal = document.getElementById(`equal`);
-const clear = document.getElementById(`clear`);
+const view = document.getElementById("view");
+const add = document.getElementById("add");
+const remove = document.getElementById("remove");
+const add2 = document.getElementById("add2");
+const add3 = document.getElementById("add3");
+const remove2 = document.getElementById("remove2");
+const remove3 = document.getElementById("remove3");
+const reset = document.getElementById("reset");
 
-function updateCalculation (num){
-    return calculation += num;
+let cart = 0;
+
+function updateCart(num){
+    return cart += num;
 };
 
-let calculation = '';
-
-num1.addEventListener(`click`, function(e){
+view.addEventListener(`click`, function(e)      {
     e.preventDefault();
 
-    updateCalculation('1');
-    console.log(calculation);
-})
-num2.addEventListener(`click`, function(e){
+    console.log(`Your Cart Quantity is: ${cart}`);
+});
+
+add.addEventListener(`click`, function(e)       {
     e.preventDefault();
 
-    // calculation += '2'
-    updateCalculation('2')
-    console.log(calculation);
-})
-num3.addEventListener(`click`, function(e){
+    cart++
+
+    console.log(`Cart Quantity: ${cart}`);
+});
+
+remove.addEventListener(`click`, function(e)    {
     e.preventDefault();
 
-    // calculation += '3'
-    updateCalculation('3')
-    console.log(calculation);
-})
-num4.addEventListener(`click`, function(e){
+    if(cart > 0){
+        cart--;
+    }
+    else{
+        console.log("Cart is empty now");
+        // console.log(cart);
+    }
+
+    console.log(`Cart Quantity: ${cart}`);
+});
+
+add2.addEventListener(`click`, function(e)      {
     e.preventDefault();
 
-    // calculation += '4'
-    updateCalculation('4');
-    console.log(calculation);
-})
-num5.addEventListener(`click`, function(e){
+    cart += 2;
+
+    console.log(`Cart Quantity: ${cart}`);
+});
+
+add3.addEventListener(`click`, function(e)      {
+    e.preventDefault();
+    
+    cart += 3;
+    
+    console.log(`Cart Quantity: ${cart}`);
+    
+});
+
+remove2.addEventListener(`click`, function(e)   {
+    e.preventDefault();
+    
+    if(cart > 1){
+        console.log("removing 2")
+        cart -= 2;
+    }
+    else if(cart === 1){
+        console.log("removing 1")
+        cart -= 1;
+    }
+    else{
+        console.log("Cart is empty now");
+    }
+    
+    console.log(`Cart Quantity: ${cart}`);
+
+});
+
+
+remove3.addEventListener(`click`, function(e)   {
     e.preventDefault();
 
-    // calculation += '5'
-    updateCalculation('5');
-    console.log(calculation);
-})
-num6.addEventListener(`click`, function(e){
+    if(cart > 2){
+        console.log("removing 3")
+        cart -= 3;
+    }
+    else if(cart === 2){
+        console.log("removing 2")
+        cart -= 2;
+    }
+    else if(cart === 1){
+        console.log("removing 1")
+        cart--;
+    }
+    else{
+        console.log("cart is empty now.");
+        // console.log(`Cart quantity: ${cart}`);
+    }
+    console.log(`Cart quantity: ${cart}`);
+});
+
+reset.addEventListener(`click`, function(e)     {
     e.preventDefault();
-
-    // calculation += '6'
-    updateCalculation('6')
-    console.log(calculation);
-})
-num7.addEventListener(`click`, function(e){
-    e.preventDefault();
-
-    // calculation += '7'
-    updateCalculation('7')
-    console.log(calculation);
-})
-num8.addEventListener(`click`, function(e){
-    e.preventDefault();
-
-    // calculation += '8'
-    updateCalculation('8')
-    console.log(calculation);
-})
-num9.addEventListener(`click`, function(e){
-    e.preventDefault();
-
-    // calculation += '9'
-    updateCalculation('9');
-    console.log(calculation);
-})
-num0.addEventListener(`click`, function(e){
-    e.preventDefault();
-
-    // calculation += '0'
-    updateCalculation('0');
-    console.log(calculation);
-})
-plus.addEventListener(`click`, function(e){
-    e.preventDefault();
-
-    // calculation += ' + '
-    updateCalculation(' + ');
-    console.log(calculation);
-})
-minus.addEventListener(`click`, function(e){
-    e.preventDefault();
-
-    // calculation += ' - '
-    updateCalculation(' - ');
-    console.log(calculation);
-})
-multiply.addEventListener(`click`, function(e){
-    e.preventDefault();
-
-    // calculation += ' * '
-    updateCalculation(' * ');
-    console.log(calculation);
-})
-divide.addEventListener(`click`, function(e){
-    e.preventDefault();
-
-    // calculation += ' / '
-    updateCalculation(' / ');
-    console.log(calculation);
-})
-equal.addEventListener(`click`, function(e){
-    e.preventDefault();
-
-    calculation = eval(calculation);
-    console.log(calculation);
-})
-clear.addEventListener(`click`, function(e){
-    e.preventDefault();
-
-    calculation = ''
-    console.log('cleared');
-})
-
+    cart = 0;
+    console.log("Cart is reseted");
+    console.log(`Cart Quantity: ${cart}`);
+});
