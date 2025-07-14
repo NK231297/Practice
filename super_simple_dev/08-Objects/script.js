d5552b2b2e4bec0c811e68888610c23e0f0fc3c5
NK231297 <nk231297@gmail.com>
2025-07-14 11:05:20 +0530

    Auto backup at 14-07-2025 11:05:20.52

--- super_simple_dev/08-Objects/script.js
+++ super_simple_dev/08-Objects/script.js
@@ -6,9 +6,12 @@ const reset = document.getElementById("reset");
 
 let roundsAsk = prompt("Please tell how much rounds you want to play");
 
-let win = 0;
-let lose = 0;
-let draw = 0;
+let results = {
+    win: 0,
+    lose: 0,
+    draw: 0
+
+}
 let roundscomplete = 0;
 
 let totalRounds = roundsAsk;
@@ -23,62 +26,75 @@ function desicion (num){
     roundscomplete++
     let remainingRounds = totalRounds - roundscomplete;
 
-    console.log(`total rounds: ${totalRounds}`)
-    console.log(`rounds complete: ${roundscomplete}`)
-    console.log(`rounds remaining: ${remainingRounds}`)
+    // console.log(`total rounds: ${totalRounds}`)
+    // console.log(`rounds complete: ${roundscomplete}`)
+    // console.log(`rounds remaining: ${remainingRounds}`)
 
 
-    if(remainingRounds > 0){
+    if(roundscomplete <= totalRounds){
         if(hMoveNumber === 1 && cMoveNumber === 2){
-            lose++;
+            results.lose++;
             console.log("You: Rock");
             console.log("Computer: Paper");
             console.log("You Lose!");
         } //you lose
         else if(hMoveNumber === 1 && cMoveNumber === 3){
-            win++;
+            results.win++;
             console.log("You: Rock");
             console.log("Computer: Scissor");
             console.log("You Win!")
         } //you win
         else if(hMoveNumber === 2 && cMoveNumber === 1){
-            win++;
+            results.win++;
             console.log("You: Paper");
             console.log("Computer: Rock");
             console.log("You Win!");
         } //you win
         else if(hMoveNumber === 2 && cMoveNumber === 3){
-            lose++;
+            results.lose++;
             console.log("You: Paper");
             console.log("Computer: Scissor");
             console.log("You Lose!");
         } //you lose
         else if(hMoveNumber === 3 && cMoveNumber === 1){
-            lose++;
+            results.lose++;
             console.log("You: Scissor");
             console.log("Computer: Rock");
-            console/log("You Lose!");
+            console.log("You Lose!");
         } //you lose
         else if(hMoveNumber === 3 && cMoveNumber === 2){
-            win++;
+            results.win++;
             console.log("You: Scissor");
             console.log("Computer: Paper");
             console.log("You Win!");
         } //you win
-        else{
-            draw++;
+        else if(hMoveNumber === 1 && cMoveNumber === 1){
+            results.draw++;
+            console.log("You: Rock");
+            console.log("Computer: Rock");
             console.log("Game Tie!");
         } //Draw
+        else if(hMoveNumber === 2 && cMoveNumber === 2){
+            results.draw++;
+            console.log("You: Paper");
+            console.log("Computer: Paper");
+            console.log("Game Tie!");
+        } //Draw
+        else if(hMoveNumber === 3 && cMoveNumber === 3){
+            results.draw++;
+            console.log("You: Scissor");
+            console.log("Computer: Scissor");
+            console.log("Game Tie!");
+        } //Draw
+        else{
+            console.log("Something went wrong")
+        }
     }
     else{
-        console.log(`Win: ${win}`);
-        console.log(`Lose: ${lose}`);
-        console.log(`Win: ${draw}`);
+        console.log("Game End.")
     }
+    
 }
-
-
-
 
 rock.addEventListener(`click`, function(e){
     e.preventDefault();
@@ -97,6 +113,11 @@ scissor.addEventListener(`click`, function(e){
 })
 result.addEventListener(`click`, function(e){
     e.preventDefault();
-
     
+    console.log(results);
+})
+
+reset.addEventListener(`click`, function(e){
+    e.preventDefault();
+    location.reload();
 })
