4d40266b75882bbf013f94733e3123b27497a931
NK231297 <nk231297@gmail.com>
2025-07-14 21:13:28 +0530

    Auto backup at 14-07-2025 21:13:28.80

---
+++ super_simple_dev/08-Objects/rough/script.js
@@ -0,0 +1,21 @@
+/*
+const object1 = {};
+
+object1.name = 'Nikhil';
+object1.age = '24';
+object1.weight = '55kg';
+object1.gender = 'Male';
+
+console.log(object1);
+*/
+
+const object1 = {
+    name: 'Nikhil Kumar',
+    age: '24',
+    gender: 'Male',
+    weight: '55 kg'
+};
+
+const {name} = object1
+
+// console.log(name);
--- super_simple_dev/08-Objects/script.js
+++ super_simple_dev/08-Objects/script.js
@@ -6,17 +6,15 @@ const reset = document.getElementById("reset");
 
 let roundsAsk = prompt("Please tell how much rounds you want to play");
 
-let results = {
-    win: 0,
-    lose: 0,
-    draw: 0
+let results = JSON.parse(localStorage.getItem('results')) || {
+        win: 0,
+        lose: 0,
+        draw: 0
+    }
 
-}
 let roundscomplete = 0;
 
 let totalRounds = roundsAsk;
-
-
 
 function desicion (num){
     let cMoveNumber = Math.floor(Math.random() * 3) + 1;
@@ -33,58 +31,121 @@ function desicion (num){
 
     if(roundscomplete <= totalRounds){
         if(hMoveNumber === 1 && cMoveNumber === 2){
-            results.lose++;
-            console.log("You: Rock");
-            console.log("Computer: Paper");
-            console.log("You Lose!");
+            results.lose += 1;
+            const moveLog = {
+                You: 'Rock',
+                Computer: 'Paper',
+                roundResult: 'You Lose!'
+            };
+            console.log(moveLog);
+            
+            //console.log("You: Rock");
+            //console.log("Computer: Paper");
+            //console.log("You Lose!");
         } //you lose
         else if(hMoveNumber === 1 && cMoveNumber === 3){
-            results.win++;
-            console.log("You: Rock");
-            console.log("Computer: Scissor");
-            console.log("You Win!")
+            results.win += 1;
+            const moveLog = {
+                You: 'Rock',
+                Computer: 'Scissor',
+                roundResult: 'You Win!'
+            };
+            console.log(moveLog);
+            
+            //console.log("You: Rock");
+            //console.log("Computer: Scissor");
+            //console.log("You Win!")
         } //you win
         else if(hMoveNumber === 2 && cMoveNumber === 1){
-            results.win++;
-            console.log("You: Paper");
-            console.log("Computer: Rock");
-            console.log("You Win!");
+            results.win += 1;
+            const moveLog = {
+                You: 'Paper',
+                Computer: 'Rock',
+                roundResult: 'You Win!'
+            };
+            console.log(moveLog);
+            
+            //console.log("You: Paper");
+            //console.log("Computer: Rock");
+            //console.log("You Win!");
         } //you win
         else if(hMoveNumber === 2 && cMoveNumber === 3){
-            results.lose++;
-            console.log("You: Paper");
-            console.log("Computer: Scissor");
-            console.log("You Lose!");
+            results.lose += 1;
+            const moveLog = {
+                You: 'Paper',
+                Computer: 'Scissor',
+                roundResult: 'You Lose!'
+            };
+            console.log(moveLog);
+            
+            //console.log("You: Paper");
+            //console.log("Computer: Scissor");
+            //console.log("You Lose!");
         } //you lose
         else if(hMoveNumber === 3 && cMoveNumber === 1){
-            results.lose++;
-            console.log("You: Scissor");
-            console.log("Computer: Rock");
-            console.log("You Lose!");
+            results.lose += 1;
+            const moveLog = {
+                You: 'Scissor',
+                Computer: 'Rock',
+                roundResult: 'You Lose!'
+            };
+            console.log(moveLog);
+            
+            //console.log("You: Scissor");
+            //console.log("Computer: Rock");
+            //console.log("You Lose!");
         } //you lose
         else if(hMoveNumber === 3 && cMoveNumber === 2){
-            results.win++;
-            console.log("You: Scissor");
-            console.log("Computer: Paper");
-            console.log("You Win!");
+            results.win += 1;
+            const moveLog = {
+                You: 'Scissor',
+                Computer: 'Paper',
+                roundResult: 'You Win!'
+            };
+            console.log(moveLog);
+            
+            //console.log("You: Scissor");
+            //console.log("Computer: Paper");
+            //console.log("You Win!");
         } //you win
         else if(hMoveNumber === 1 && cMoveNumber === 1){
-            results.draw++;
-            console.log("You: Rock");
-            console.log("Computer: Rock");
-            console.log("Game Tie!");
+            results.draw += 1;
+            const moveLog = {
+                You: 'Rock',
+                Computer: 'Rock',
+                roundResult: 'Game Tie!'
+            };
+            console.log(moveLog);
+            
+            //console.log("You: Rock");
+            //console.log("Computer: Rock");
+            //console.log("Game Tie!");
         } //Draw
         else if(hMoveNumber === 2 && cMoveNumber === 2){
-            results.draw++;
-            console.log("You: Paper");
-            console.log("Computer: Paper");
-            console.log("Game Tie!");
+            results.draw += 1;
+            const moveLog = {
+                You: 'Paper',
+                Computer: 'Paper',
+                roundResult: 'Game Tie!'
+            };
+            console.log(moveLog);
+            
+            //console.log("You: Paper");
+            //console.log("Computer: Paper");
+            //console.log("Game Tie!");
         } //Draw
         else if(hMoveNumber === 3 && cMoveNumber === 3){
-            results.draw++;
-            console.log("You: Scissor");
-            console.log("Computer: Scissor");
-            console.log("Game Tie!");
+            results.draw += 1;
+            const moveLog = {
+                You: 'Scissor',
+                Computer: 'Scissor',
+                roundResult: 'Game Tie!'
+            };
+            console.log(moveLog);
+            
+            //console.log("You: Scissor");
+            //console.log("Computer: Scissor");
+            //console.log("Game Tie!");
         } //Draw
         else{
             console.log("Something went wrong")
@@ -94,7 +155,9 @@ function desicion (num){
         console.log("Game End.")
     }
     
+    localStorage.setItem("results", JSON.stringify(results));
 }
+
 
 rock.addEventListener(`click`, function(e){
     e.preventDefault();
@@ -103,12 +166,12 @@ rock.addEventListener(`click`, function(e){
 })
 paper.addEventListener(`click`, function(e){
     e.preventDefault();
-
+    
     desicion(2);
 })
 scissor.addEventListener(`click`, function(e){
     e.preventDefault();
-
+    
     desicion(3);
 })
 result.addEventListener(`click`, function(e){
@@ -117,7 +180,10 @@ result.addEventListener(`click`, function(e){
     console.log(results);
 })
 
-reset.addEventListener(`click`, function(e){
-    e.preventDefault();
+reset.addEventListener(`click`, function(){
+    // e.preventDefault();
+
+    localStorage.removeItem('results');
+
     location.reload();
 })
