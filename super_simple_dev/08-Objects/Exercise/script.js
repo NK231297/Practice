3b5bf0985041984ca72df4805eba520835006605
NK231297 <nk231297@gmail.com>
2025-07-15 11:01:09 +0530

    Auto backup at 15-07-2025 11:01:08.95

--- super_simple_dev/08-Objects/Exercise/script.js
+++ super_simple_dev/08-Objects/Exercise/script.js
@@ -7,6 +7,12 @@ function comparePrice (product1, product2){
     };
 }
 
+function isSameProduct(product1, product2){
+    if(product1.name === product2.name || product1['price ($)'] === product2['price ($)']){
+        return true;
+    }
+    else{return false};
+};
 
 const product1 = {
     name: 'Basketball',
@@ -14,6 +20,12 @@ const product1 = {
 };
 
 const product2 = {
-    name: 'Cricket bat',
-    'price ($)': 4499 / 100
+    name:'Cricket bat',
+    'price ($)':4499 / 100
 };
+
+const product3 = {
+    name: 'Basketball',
+    'price ($)': 2095 / 100
+};
+
---
+++ super_simple_dev/08-Objects/calculator with localStorage/script.js
@@ -0,0 +1,123 @@
+const numpad = {
+    num1: document.getElementById('num1'),
+    num2: document.getElementById('num2'),
+    num3: document.getElementById('num3'),
+    num4: document.getElementById('num4'),
+    num5: document.getElementById('num5'),
+    num6: document.getElementById('num6'),
+    num7: document.getElementById('num7'),
+    num8: document.getElementById('num8'),
+    num9: document.getElementById('num9'),
+    num0: document.getElementById('num0')
+};
+
+const equations = {
+    plus: document.getElementById('plus'),
+    minus: document.getElementById('minus'),
+    multiply: document.getElementById('multiply'),
+    divide: document.getElementById('divide'),
+    equal: document.getElementById('equal')
+};
+
+const essential = {
+    clear: document.getElementById('clear')
+};
+
+let calculation = '';
+
+function num (num){
+    calculation += num;
+    console.log(calculation);
+};
+
+numpad.num1.addEventListener('click', function(e){
+    e.preventDefault();
+    num(1);
+});
+
+numpad.num2.addEventListener('click', function(e){
+    e.preventDefault();
+    num(2);
+});
+
+numpad.num3.addEventListener('click', function(e){
+    e.preventDefault();
+    num(3);
+});
+
+numpad.num4.addEventListener('click', function(e){
+    e.preventDefault();
+    num(4);
+});
+
+numpad.num5.addEventListener('click', function(e){
+    e.preventDefault();
+    num(5);
+});
+
+numpad.num6.addEventListener('click', function(e){
+    e.preventDefault();
+    num(6);
+});
+
+numpad.num7.addEventListener('click', function(e){
+    e.preventDefault();
+    num(7);
+});
+
+numpad.num8.addEventListener('click', function(e){
+    e.preventDefault();
+    num(8);
+});
+
+numpad.num9.addEventListener('click', function(e){
+    e.preventDefault();
+    num(9);
+});
+
+numpad.num0.addEventListener('click', function(e){
+    e.preventDefault();
+    num(0);
+});
+
+equations.plus.addEventListener('click', function(e){
+    e.preventDefault();
+
+    calculation += ' + ';
+    console.log(calculation);
+});
+
+equations.minus.addEventListener('click', function(e){
+    e.preventDefault();
+
+    calculation += ' - ';
+    console.log(calculation);
+});
+
+equations.multiply.addEventListener('click', function(e){
+    e.preventDefault();
+
+    calculation += ' * ';
+    console.log(calculation);
+});
+
+equations.divide.addEventListener('click', function(e){
+    e.preventDefault();
+
+    calculation += ' / ';
+    console.log(calculation);
+})
+
+equations.equal.addEventListener('click', function(e){
+    e.preventDefault();
+
+    calculation = eval(calculation)
+    console.log(calculation);
+});
+
+essential.clear.addEventListener('click', function(e){
+    e.preventDefault();
+
+    calculation = ''
+    console.log(`Calculation cleared ${calculation}`);
+});
