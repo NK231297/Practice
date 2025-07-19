dbac276acb5d6ece8bcb87e0f99a6a244ffc9e77
NK231297 <nk231297@gmail.com>
2025-07-19 23:08:45 +0530

    Auto backup at 19-07-2025 23:08:45.36

--- super_simple_dev/09-DOM/projects with DOM/Rock paper scissor/script.js
+++ super_simple_dev/09-DOM/projects with DOM/Rock paper scissor/script.js
@@ -1,22 +1,123 @@
+const moves = {
+    rock: document.querySelector('#rock'),
+    paper: document.querySelector('#paper'),
+    scissor: document.querySelector('#scissor'),
+};
+const essential = {
+    reset: document.querySelector('reset')
+};
+
 const score = {
     win: 0,
     lose: 0,
-    draw: 0,
+    draw: 0
 };
 
-function function1 (num){
-    let userMove = num;
+let scoreDOM = document.querySelector('#score');
+let moveDOM = document.querySelector('#move');
 
-    let compMove = Math.floor(Math.random() * 3) + 1;
-
-    let result = userMove === compMove ? ('Draw', score.draw++) : 
-    userMove === 1 && compMove === 2 ? ('Lose', score.lose++) : 
-    userMove === 1 && compMove === 3 ? ('Win', score.win++) : 
-    userMove === 2 && compMove === 1 ? ('Win', score.win++) : 
-    userMove === 2 && compMove === 3 ? ('Lose', score.lose++) : 
-    userMove === 3 && compMove === 1 ? ('Lose', score.lose++) : 
-    userMove === 3 && compMove === 2 ? ('Win', score.win++) : 'error';
-
-    localStorage.setItem('rock, paper, scissor', JSON.stringify(score));
-    
+const move = {
+    youMove: '',
+    comMove: ''
 };
+
+
+function function1(num){
+    let humanMove = num;
+    let compumove = Math.floor(Math.random() * 3) + 1;
+
+    if( humanMove === 1 && compumove === 2 ){
+        score.lose ++;
+        console.log('You: Rock')
+        console.log('Computer: Paper')
+        console.log('lose')
+        move.youMove = 'Rock'
+        move.comMove = 'Paper'
+    }//lose
+    else if( humanMove === 1 && compumove === 3 ){
+        score.win++;
+        console.log('You: Rock')
+        console.log('Computer: Scissor')
+        console.log('win');
+        move.youMove = 'Rock';
+        move.comMove = 'Scissor';
+    }//win
+    else if( humanMove === 2 && compumove === 1 ){
+        score.win++;
+        console.log('You: Paper')
+        console.log('Computer: Rock')
+        console.log('win')
+        move.youMove = 'Paper'
+        move.comMove = 'Rock'
+    }//win
+    else if( humanMove === 2 && compumove === 3 ){
+        score.lose++;
+        console.log('You: Paper')
+        console.log('Computer: Scissor')
+        console.log('lose')
+        move.youMove = 'Paper'
+        move.comMove = 'Scissor'
+    }//lose
+    else if( humanMove === 3 && compumove === 1 ){
+        score.lose++;
+        console.log('You: Scissor')
+        console.log('Computer: Rock')
+        console.log('lose')
+        move.youMove = 'Scissor'
+        move.comMove = 'Rock'
+        
+    }//lose
+    else if( humanMove === 3 && compumove === 2 ){
+        score.win++;
+        console.log('You: Scissor')
+        console.log('Computer: Paper')
+        console.log('win')
+        move.youMove = 'Scissor'
+        move.comMove = 'Paper'
+    }//win
+    else{
+        score.draw++;
+        console.log('Draw!');
+        if(humanMove === 1 && compumove === 1){
+            move.youMove = 'Rock'
+            move.comMove = 'Rock'
+        }
+        else if (humanMove === 2 && compumove === 2){
+            move.youMove = 'Paper'
+            move.comMove = 'Paper'
+        }
+        else if (humanMove === 3 && compumove === 3){
+            move.youMove = 'Scissor'
+            move.comMove = 'Scissor'
+        }
+        else{
+            console.log('error');
+        }
+    }//draw
+
+    scoreDOM.innerText = `Win: ${score.win} | Lose: ${score.lose} | Draw: ${score.draw}`
+    moveDOM.innerText = `You: ${move.youMove} | Computer: ${move.comMove}`
+
+}
+
+
+
+
+moves.rock.addEventListener(`click`, function(e){
+e.preventDefault();
+function1(1)
+// console.log('Rock');
+})
+moves.paper.addEventListener(`click`, function(e){
+e.preventDefault();
+function1(2)
+// console.log('Paper');
+})
+moves.scissor.addEventListener(`click`, function(e){
+e.preventDefault();
+function1(3)
+// console.log('Scissor');
+})
+
+
+
