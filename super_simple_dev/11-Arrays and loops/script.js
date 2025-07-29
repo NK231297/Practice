23cd06f07cf9edfc75f41cfc7a8a4ccaa9172137
NK231297 <nk231297@gmail.com>
2025-07-29 21:49:50 +0530

    Auto backup at 29-07-2025 21:49:50.47

--- super_simple_dev/11-Arrays and loops/script.js
+++ super_simple_dev/11-Arrays and loops/script.js
@@ -16,3 +16,53 @@ function function1(){
 addToDo.addEventListener('click', function(){
     function1();
 })
+
+/*
+let index = 1;
+
+while(index <= 5){
+    console.log(index);
+    index++;
+}
+*/
+/*
+const list = [
+    'Make dinner.',
+    'Wash dishes', 
+    'Watch youtube.'
+];
+
+console.log(list.length);
+
+for(let i = 0; i < list.length; i++){
+    let listItem = list[i];
+    console.log(i);
+}
+*/
+/*
+const array1 = [1, 1, 3];
+
+let total = 0;
+
+for(let i = 0; i < array1.length; i++){
+    let num = array1[i];
+
+    total = total + num;
+}
+
+console.log(total);
+*/
+
+const array1 = [1, 1, 3];
+
+let array2 = [];
+
+for(let i = 0; i < array1.length; i++){
+    let abc = array1[i];
+
+    let bcd = array1[i] += array1[i];
+
+    array2.push(bcd);
+}
+
+console.log(array2);
