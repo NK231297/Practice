96c6982fd8dfac9ee5a9f9323a21e20753295186
NK231297 <nk231297@gmail.com>
2025-08-03 10:29:13 +0530

    Auto backup at 03-08-2025 10:29:13.61

--- super_simple_dev/11-Arrays and loops/todo project/script.js
+++ super_simple_dev/11-Arrays and loops/todo project/script.js
@@ -2,12 +2,17 @@ const todoInput     = document.querySelector('#todoInput');
 const addBT         = document.querySelector('#addBT');
 const deleteBT      = document.querySelector('#deleteBT');
 const todoRender    = document.querySelector('#todoRender');
+const dueDate       = document.querySelector('#dueDate');
 
 
 const todoArray = [];
 
 function function1(){
-    todoArray.push(todoInput.value);
+    
+    todoArray.push({
+        todo: todoInput.value,
+        date: dueDate.value
+    });
     todoInput.value = '';
     
 }
@@ -16,7 +21,7 @@ function function2(){
     todoRender.innerHTML = ``;
 
     for(let i = 0; i < todoArray.length; i++){
-    let html = `<p>${todoArray[i]} <button onclick="function3(${i})">Delete</button> </p>`
+    let html = `<p> <span id = "todo">${todoArray[i].todo}</span> <span id = "date">${todoArray[i].date}</span> <button id = "deleteBT" onclick="function3(${i})">Delete</button> </p>`
     
         todoRender.innerHTML += html;
     }
