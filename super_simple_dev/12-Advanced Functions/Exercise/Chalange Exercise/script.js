0b98bbf02f72efb98bf26c7c735f577a5842e351
NK231297 <nk231297@gmail.com>
2025-08-20 12:01:28 +0530

    Auto backup at 20-08-2025 12:01:27.98

---
+++ super_simple_dev/12-Advanced Functions/Exercise/Chalange Exercise/script.js
@@ -0,0 +1,222 @@
+const moves = {
+    rock: document.querySelector('#rock'),
+    paper: document.querySelector('#paper'),
+    scissor: document.querySelector('#scissor'),
+};
+const essential = {
+    reset: document.querySelector('#reset'),
+    autoPlayBT: document.querySelector('#autoPlay'),
+    rstConfirmElement: document.querySelector('.resetConfirm')
+};
+
+const DOMText = {
+    scoreDOM1: document.querySelector('#winNum'),
+    scoreDOM2: document.querySelector('#loseNum'),
+    scoreDOM3: document.querySelector('#drawNum'),
+    moveDOM1: document.querySelector('#you'),
+    moveDOM2: document.querySelector('#computer'),
+    desicionDOM: document.querySelector('#desicion')
+};
+
+function num(){
+    let moveNum = Math.floor(Math.random() * 3) + 1;
+    return moveNum;
+}
+
+let autoPlaying = false;
+let intervalId;
+function autoPlay(){
+    if(!autoPlaying){
+        intervalId = setInterval(function(){
+            const playerMove = num();
+            
+            function1(playerMove);
+        }, 1000)
+        autoPlaying = true;
+        essential.autoPlayBT.innerHTML = 'Turn off'
+    }
+    else{
+        clearInterval(intervalId);
+        autoPlaying = false;
+        essential.autoPlayBT.innerHTML = 'Auto Play'
+    }
+}
+
+const resetConfirm = ()=>{
+    essential.rstConfirmElement.innerHTML = `
+    <p class = "genConfMess">Do you want to delete game data ?</p>    
+    <button class = "yes"> Yes </button>
+    <button class = "no"> No </button>
+    `
+    const yes = document.querySelector('.yes');
+    const no = document.querySelector('.no');
+
+    yes.addEventListener('click', ()=>{
+        localStorage.removeItem('Index 1');
+        DOMText.moveDOM1.innerHTML = ` - `;
+        DOMText.moveDOM2.innerHTML = ` - `;
+        DOMText.scoreDOM1.innerHTML = ` - `;
+        DOMText.scoreDOM2.innerHTML = ` - `;
+        DOMText.scoreDOM3.innerHTML = ` - `;
+        DOMText.desicionDOM.innerHTML = `Desicion cleared.`;
+        essential.rstConfirmElement.innerHTML = '';
+    })
+
+    no.addEventListener('click', ()=>{
+        essential.rstConfirmElement.innerHTML = '';
+    })
+
+}
+
+
+function function1(num){
+    
+    const score = JSON.parse(localStorage.getItem('Index 1')) || {
+        win: 0,
+        lose: 0,
+        draw: 0
+    }
+
+    let humanMove = num;
+    let compumove = Math.floor(Math.random() * 3) + 1;
+
+    if( humanMove === 1 && compumove === 2 ){
+        score.lose ++;
+        console.log('You: Rock')
+        console.log('Computer: Paper')
+        console.log('lose')
+        DOMText.moveDOM1.innerHTML = '&#9994'
+        DOMText.moveDOM2.innerHTML = '&#9995'
+        DOMText.desicionDOM.innerHTML = `You lose !`
+    }//lose
+    else if( humanMove === 1 && compumove === 3 ){
+        score.win++;
+        console.log('You: Rock')
+        console.log('Computer: Scissor')
+        console.log('win');
+        DOMText.moveDOM1.innerHTML = '&#9994';
+        DOMText.moveDOM2.innerHTML = '&#9996';
+        DOMText.desicionDOM.innerHTML = `You win !`
+    }//win
+    else if( humanMove === 2 && compumove === 1 ){
+        score.win++;
+        console.log('You: Paper')
+        console.log('Computer: Rock')
+        console.log('win')
+        DOMText.moveDOM1.innerHTML = '&#9995'
+        DOMText.moveDOM2.innerHTML = '&#9994'
+        DOMText.desicionDOM.innerHTML = `You win !`
+    }//win
+    else if( humanMove === 2 && compumove === 3 ){
+        score.lose++;
+        console.log('You: Paper')
+        console.log('Computer: Scissor')
+        console.log('lose')
+        DOMText.moveDOM1.innerHTML = '&#9995'
+        DOMText.moveDOM2.innerHTML = '&#9996'
+        DOMText.desicionDOM.innerHTML = `You lose !`
+    }//lose
+    else if( humanMove === 3 && compumove === 1 ){
+        score.lose++;
+        console.log('You: Scissor')
+        console.log('Computer: Rock')
+        console.log('lose')
+        DOMText.moveDOM1.innerHTML = '&#9996'
+        DOMText.moveDOM2.innerHTML = '&#9994'
+        DOMText.desicionDOM.innerHTML = `You lose !`
+        
+    }//lose
+    else if( humanMove === 3 && compumove === 2 ){
+        score.win++;
+        console.log('You: Scissor')
+        console.log('Computer: Paper')
+        console.log('win')
+        DOMText.moveDOM1.innerHTML = '&#9996'
+        DOMText.moveDOM2.innerHTML = '&#9995'
+        DOMText.desicionDOM.innerHTML = `You win !`
+    }//win
+    else{
+        score.draw++;
+        console.log('Draw!');
+        DOMText.desicionDOM.innerHTML = `Game draw !`
+        if(humanMove === 1 && compumove === 1){
+            DOMText.moveDOM1.innerHTML = '&#9994'
+            DOMText.moveDOM2.innerHTML = '&#9994'
+        }
+        else if (humanMove === 2 && compumove === 2){
+            DOMText.moveDOM1.innerHTML = '&#9995'
+            DOMText.moveDOM2.innerHTML = '&#9995'
+        }
+        else if (humanMove === 3 && compumove === 3){
+            DOMText.moveDOM1.innerHTML = '&#9996'
+            DOMText.moveDOM2.innerHTML = '&#9996'
+        }
+        else{
+            console.log('error');
+        }
+    }//draw
+
+
+    localStorage.setItem('Index 1', JSON.stringify(score));
+
+    DOMText.scoreDOM1.innerText = score.win
+    DOMText.scoreDOM2.innerText = score.lose
+    DOMText.scoreDOM3.innerText = score.draw
+    // DOMText.moveDOM.innerHTML = `You: ${DOMText.moveDOM1.innerHTML} | Computer: ${DOMText.moveDOM2.innerHTML}`
+}
+
+
+moves.rock.addEventListener(`click`, function(e){
+e.preventDefault();
+function1(1)
+// console.log('Rock');
+})
+
+document.body.addEventListener('keydown', (e)=>{
+    
+    if(e.key === 'r' || e.key === 'R'){
+        function1(1)
+    }
+    else if(e.key === 'p' || e.key === 'P'){
+        function1(2)
+    }
+    else if(e.key === 's' || e.key === 'S'){
+        function1(3)
+    }
+    else if(e.key === 'a'){
+        autoPlay();
+    }
+    else if(e.key === 'Backspace'){
+        localStorage.removeItem('Index 1');
+        DOMText.moveDOM1.innerHTML = ` - `;
+        DOMText.moveDOM2.innerHTML = ` - `;
+        DOMText.scoreDOM1.innerHTML = ` - `;
+        DOMText.scoreDOM2.innerHTML = ` - `;
+        DOMText.scoreDOM3.innerHTML = ` - `;
+        DOMText.desicionDOM.innerHTML = `Desicion cleared.`;
+    }
+    else{
+        console.log('Valid keys to play the game are r/R, p/P, s/S')
+    }
+    
+});
+
+moves.paper.addEventListener(`click`, function(e){
+e.preventDefault();
+function1(2)
+// console.log('Paper');
+})
+
+moves.scissor.addEventListener(`click`, function(e){
+e.preventDefault();
+function1(3)
+// console.log('Scissor');
+})
+
+essential.reset.addEventListener(`click`, function(e){
+   resetConfirm();
+})
+
+essential.autoPlayBT.addEventListener('click', function(){
+    autoPlay();
+})
