8cd175c7ade76da7886ac9c2bcb8a0a6c03d339c
NK231297 <nk231297@gmail.com>
2025-11-05 11:24:04 +0530

    Manual backup on 05-11-2025 11:24:03.95

--- super_simple_dev/17-object-oriented-programming/Amazon project/scripts/amazon.js
+++ super_simple_dev/17-object-oriented-programming/Amazon project/scripts/amazon.js
@@ -103,4 +103,7 @@ function renderProductsGrid(){
   })
 };
 
-//He
+document.querySelector('.js-search-button').addEventListener('click', ()=>{
+  const search = document.querySelector('.js-search-bar').value;
+  window.location.href = `amazon.html?search=${search}`;
+});
