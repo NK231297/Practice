d36abd7ba9fa7288e4e919421d4e33a1a29bec7c
NK231297 <NK231297@gmail.com>
2025-11-25 16:36:47 +0000

    Backup update

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,62 @@ const server = http.createServer((req, res)=>{
     // console.log(req);
     // console.log(req.url, req.method ,req.headers);
 
+    if(req.url === '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`
+                <!DOCTYPE html>
+                <html lan="en">
+                    <header>
+                        <title> Home </title>
+                    </header>
+                    <body>
+                        <h1> Enter your details </h1>
+                        <h3>Enter your Name</h3>
+                        </br>
+                        <input type="text" name="" id="" placeholder="Enter your name">
+                        </br>
+                        </br>
+                        <h3>Enter your gender</h3>
+                        </br>
+                        <input type="text" id="" name="">
+                        </br>
+                        </br>
+                        <button> Submit </button>
+                    </body>
+                </html>
+            `)
+        res.end();
+        return;
+    } else if(req.url === '/products'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`
+                <!DOCTYPE html>
+                <html lan="en">
+                    <header>
+                        <title> Products </title>
+                    </header>
+                    <body>
+                        <h1> Checkout our products </h1>
+                    </body>
+                </html>
+            `)
+        res.end();
+        return;
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`
+            <!DOCTYPE html>
+            <html lan="en">
+                <header>
+                    <title> Others </title>
+                </header>
+                <body>
+                    <h1> LIKE / SHARE / SUBSCRIBE </h1>
+                </body>
+            </html>
+        `)
+
+    
     
 });
 
@@ -12,3 +68,27 @@ const PORT = 3001;
 server.listen(PORT, ()=>{
     console.log(`Server running on address http://localhost:${PORT}`);
 });
+
+/*
+res.write(`
+            <!DOCTYPE html>
+            <html lang="en">
+            <header>
+                <title> Home </title>
+            </header>
+            <body>
+                <h1>Enter your details</h1>
+                <form>
+                    <h3>Name:</h3>
+                    </br>
+                    <input type="text" placeholder="Enter your name">
+                    </br>
+                    </br>
+                    <h3>Gender:</h3>
+                    </br>
+                    <input type="text" placeholder="Enter your gender">
+                </form>
+            </body>
+            </html>
+        `)
+        */
